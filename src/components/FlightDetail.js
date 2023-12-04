4962916b7aa70095ec5a4e55907435b3d933c466
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2023-12-04 18:58:36 +0300

    Add FlightDetail component

---
+++ src/components/FlightDetail.js
@@ -0,0 +1,12 @@
+import Modal from './Modal';
+
+const FlightDetail = () => {
+  return (
+    <Modal
+      title="Flight Details"
+      children={<div>test</div>}
+    />
+  )
+}
+
+export default FlightDetail;
