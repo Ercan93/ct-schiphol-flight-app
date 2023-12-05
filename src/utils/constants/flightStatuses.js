a9a561774bab499ccdf02aebe6e938450dc9efe3
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2023-12-05 14:33:40 +0300

    Add flight status constants

---
+++ src/utils/constants/flightStatuses.js
@@ -0,0 +1,26 @@
+export const arrivingFlightStatuses = {
+  "SCH": "Flight Scheduled",
+  "AIR": "Airborne",
+  "EXP": "Expected Landing",
+  "FIR": "Flight in Dutch Airspace",
+  "LND": "Landed",
+  "FIB": "FIBAG",
+  "ARR": "Arrived Flight has been completely handled",
+  "DIV": "Diverted",
+  "CNX": "Cancelled",
+  "TOM": "Tomorrow",
+}
+
+export const departingFlightStatuses = {
+  "SCH": "Flight Scheduled",
+  "DEL": "Delayed",
+  "WIL": "Wait in Lounge",
+  "GTO": "Gate Open",
+  "BRD": "Boarding",
+  "GCL": "Gate Closing",
+  "GTD": "Gate Closed",
+  "DEP": "Departed",
+  "CNX": "Cancelled",
+  "GCH": "Gate Change",
+  "TOM": "Tomorrow",
+}
